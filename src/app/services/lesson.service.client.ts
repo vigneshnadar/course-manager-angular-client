6fe4caf992a557d204348d78e716caec0ba65496
Vignesh Nadar <vigneshnadar@Vigneshs-MacBook-Pro.local>
2018-06-13 14:14:06 -0400

    backup

--- src/app/services/lesson.service.client.ts
+++ src/app/services/lesson.service.client.ts
@@ -1,6 +1,6 @@
 export class LessonServiceClient {
   findLessonsForModule(moduleId) {
-    return fetch('http://localhost:8080/api/module/' + moduleId + '/lesson')
+    return fetch('http://localhost:8080/api/course/112/module/' + moduleId + '/lesson')
       .then(response => response.json());
   }
 }
