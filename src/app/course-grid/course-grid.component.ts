c4ab9b8aa3f930d7eb61a5c02711fd2362cb9e3a
Vignesh Nadar <vigneshnadar@Vigneshs-MacBook-Pro.local>
2018-06-18 16:55:06 -0400

    backup

--- src/app/course-grid/course-grid.component.ts
+++ src/app/course-grid/course-grid.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {CourseServiceClient} from "../services/course.service.client";
 import {Course} from "../models/coruse.model.client";
+import {UserServiceCleint} from '../services/user.service.cleint';
+import {SectionServiceClient} from '../services/section.service.client';
+import {User} from '../models/user.model.client';
 
 @Component({
   selector: 'app-course-grid',
@@ -9,13 +12,45 @@ import {Course} from "../models/coruse.model.client";
 })
 export class CourseGridComponent implements OnInit {
 
-  constructor(private service: CourseServiceClient) { }
+  constructor(private service: CourseServiceClient,
+              private userService: UserServiceCleint,
+              private sectionService: SectionServiceClient) { }
 
   courses: Course[] = [];
-
+  enrolledCourses: Course[] = [];
+  otherCourses: Course[] = [];
+  user: User = new User();
+  username;
+  password;
+  sections = [];
   ngOnInit() {
     this.service.findAllCourses()
       .then(courses => this.courses = courses);
+
+    this.userService.profile()
+      .then(user => this.user = user);
+
+    this.sectionService
+      .findSectionsForStudent()
+      .then(sections => this.sections = sections);
+    for (let i = 0; i < this.courses.length; i++) {
+      for (let j = 0; j < this.sections.length; j++) {
+        if (this.sections[j].courseId === this.courses[i].id){
+          this.enrolledCourses.push(this.courses[i]);
+        } else {
+          this.otherCourses.push(this.courses[i]);
+        }
+      }
+    }// end of fr
+
+    console.log('courses');
+    console.log(this.courses);
+    console.log('section');
+    console.log(this.sections);
+    console.log('enrolled');
+    console.log(this.enrolledCourses);
+    console.log('unenroll');
+    console.log(this.otherCourses);
   }
 
 }
